c806cb084cda3dfd657551be4a14b6c2c075232d
Anatoliy Bilenko <anatoliy.bilenko@seagate.com>
2019-03-14 19:47:10 +0200

    first commit

---
+++ Main.elm
@@ -0,0 +1,60 @@
+module Main exposing (main)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Time
+
+
+
+type alias Model = { timer : Int , interval : Int, stopped : Bool}
+
+init : Model
+init = { timer = 0, interval = 100, stopped = False }
+
+type Msg
+  = TimeUpdate Time.Posix
+  | ClickReset
+  | ClickToggle
+  | ChangeSelection String
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        TimeUpdate t -> (if not model.stopped && model.timer < 100
+                         then
+                             { model | timer = model.timer + 1 }
+                         else
+                             model, Cmd.none)
+        ClickReset -> ({ model | timer = 0 }, Cmd.none)
+        ClickToggle -> ({ model | stopped = not model.stopped } , Cmd.none)
+        ChangeSelection i -> ({ model | interval = Maybe.withDefault 200 <| String.toInt i }, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ text "Progress"
+    , input [ value (String.fromInt model.timer)] []
+    , button [ onClick ClickReset ] [ text "Reset" ]
+    , select [onInput ChangeSelection]
+        [ option [value "100"] [text "100"]
+        , option [value "200"] [text "200"]
+        , option [value "500"] [text "500"]
+        , option [value "999"] [text "999"]
+        ]
+    , button [ onClick ClickToggle ] [ text "Toggle" ]
+    ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every (toFloat model.interval) TimeUpdate
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \flags -> ( init, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
